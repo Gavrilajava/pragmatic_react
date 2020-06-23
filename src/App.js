32b62f3bd139bd238af77c5f7b04098e668f1735
Gavrilajava <georgy.gavrilchik@gmail.com>
2020-06-23 10:52:13 -0500

    starting pont

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import './App.css';
-import CardBrowser from './components/CardBrowser'
+
 
 function App() {
-  return (
-    <CardBrowser/>
-  );
+
 }
 
 export default App;
